ee4b348efbd64b94c1827ffd2854ffbf46cf0977
Pheba Colfer <phebajcolfer@gmail.com>
2023-05-04 16:43:59 -0400

    GitHub pages work

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
  module.exports = {
   siteMetadata: {
     title: `Activity Planner`,
-    pathPrefix: "/outings",
+    pathPrefix: "/CSC496FinalProject",
   },
   plugins: [    
   "gatsby-plugin-image",
